39fd7ad596ce07aee61b02df8272138fa0a32c4f
Naoto Morishita <mori.nto@outlook.jp>
2025-11-03 13:00:08 +0900

    feat: set background-image

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
 import { Button } from "@/components/ui/button";
+import bgImage from "@/assets/Hero-Background-notecode.svg";
 
 function App() {
-  return <Button>Click me</Button>;
+  return (
+    <div
+      className="w-screen min-h-screen bg-size-[100%] bg-no-repeat"
+      style={{ backgroundImage: `url(${bgImage})` }}
+    >
+      <Button>Click me</Button>
+    </div>
+  );
 }
 
 export default App;
