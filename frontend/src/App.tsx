ff4b1c6fb598c899662d1f5be4ab85a1554a96fe
Naoto Morishita <mori.nto@outlook.jp>
2025-11-04 10:27:24 +0900

    feat: Add Button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
-import { Button } from "@/components/ui/button";
 import bgImage from "@/assets/Hero-Background-notecode.svg";
 import { Header } from "./components/common/headet";
+import { CodeCard } from "./components/CodeCard";
 
 function App() {
   return (
@@ -8,9 +8,11 @@ function App() {
       className="w-screen min-h-screen bg-size-[100%] bg-no-repeat"
       style={{ backgroundImage: `url(${bgImage})` }}
     >
-      <div className="pt-8 pb-16">
+      <div className="pt-8 pb-16 flex flex-col items-center">
         <Header />
-        <Button>Click me</Button>
+        <div className="max-w-[720px] w-full">
+          <CodeCard />
+        </div>
       </div>
     </div>
   );
