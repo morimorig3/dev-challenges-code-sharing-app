b711492b7acb465685f2b483c9b41dd7c158c70a
Naoto Morishita <mori.nto@outlook.jp>
2025-11-06 12:59:34 +0900

    feat: Controllerの実装

--- backend/src/snippets/snippets.controller.ts
+++ backend/src/snippets/snippets.controller.ts
@@ -1,15 +1,28 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+} from '@nestjs/common';
 import { CreateSnippetDto } from './dto/create-snippet.dto';
+import { SnippetsService } from './snippets.service';
+import { Snippet } from './entities/snippets.entity';
 
 @Controller('snippets')
 export class SnippetsController {
+  constructor(private readonly snippetsService: SnippetsService) {}
+
   @Get(':id')
-  get(@Param('id') id: string) {
-    return id;
+  async findOne(@Param('id') id: string): Promise<Snippet> {
+    return await this.snippetsService.findOne(id);
   }
 
   @Post()
-  create(@Body() createSnippetDto: CreateSnippetDto) {
-    return createSnippetDto;
+  @HttpCode(HttpStatus.CREATED)
+  async create(@Body() createSnippetDto: CreateSnippetDto): Promise<Snippet> {
+    return await this.snippetsService.create(createSnippetDto);
   }
 }
